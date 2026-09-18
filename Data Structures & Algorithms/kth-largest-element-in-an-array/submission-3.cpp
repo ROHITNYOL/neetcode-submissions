61e4dec6fa5c2e36107b830cd72e9a06f8c28618
Rohit Nyol <159729739+ROHITNYOL@users.noreply.github.com>
2026-09-18 15:07:49 +0530

    Add: kth-largest-element-in-an-array - submission-3

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-3.cpp
@@ -0,0 +1,30 @@
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+        
+//         priority_queue<int, vector<int> , greater<int> > pq;
+
+//         for(int num : nums){
+
+//             if(pq.size()<k) pq.push(num);
+//             else{
+//                 if(pq.top()<num){
+//                     pq.pop();
+//                     pq.push(num);
+//                 }
+//             } 
+//         } 
+
+//         return pq.top();
+//     }
+// };
+
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        
+        nth_element(nums.begin(), nums.end() - k, nums.end()); // STL, for kth largest nums.end() - k, and for kth smallest nums.begin() + k
+        return nums[nums.size() - k];
+    }
+};
